ef79e1e825c15a867dbaff1c308f56c5120597af
vamseeuxd <vamsee.uxd@gmail.com>
2024-08-01 21:28:39 +0530

    Working on Expenses Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -196,7 +196,6 @@ export class AppComponent {
 
   expenses: IItem[] = [];
 
-  /* prettier-ignore */
   pages: IPage[] = [
     {
       signlerName: "Flat",
@@ -204,77 +203,176 @@ export class AppComponent {
       name: "Flats",
       items: this.flats,
       formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "New Flat Name", required: true }
-      ],
-      defaultValues: { name: '' },
-      itemLabelCallBack: (item: IItem): string => {
-        return item.name;
-      },
-      onFormChange: (form: NgForm, valueChanged: string): void => {}
-    },
-    {
-      signlerName: "Floor", id: "floors", name: "Floors", items: this.floors,
-      formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "New Floor Name", required: false }
-      ],
-      defaultValues: { name: '' },
-      itemLabelCallBack: (item: IItem): string => {
-        return item.name;
-      },
-      onFormChange: (form: NgForm, valueChanged: string): void => {}
-    },
-    {
-      signlerName: "Expense", id: "expenses", name: "Expenses", items: this.expenses,
-      formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "New Expense Name", required: false },
-        { type: "dropdown", id: "floor", name: "floor", defaultValue: "", dataProvider: () => this.floors, label: "Inventory Item Related Floor", required: true },
         {
-          type: "dropdown", id: "inventoryItem", name: "inventoryItem", defaultValue: "", 
-          dataProvider: (form: NgForm) => {
-            return this.inventory.filter(i => i.floor == form.value.floor);
-          }, label: "Inventory Item", required: true
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "New Flat Name",
+          required: true,
         },
-        { type: "number", id: "amount", name: "amount", defaultValue: null, dataProvider: () => [], label: "Amount in ₹", required: true },
       ],
-      defaultValues: { name: '' },
+      defaultValues: { name: "" },
+      itemLabelCallBack: (item: IItem): string => {
+        return item.name;
+      },
+      onFormChange: (form: NgForm, valueChanged: string): void => {},
+    },
+    {
+      signlerName: "Floor",
+      id: "floors",
+      name: "Floors",
+      items: this.floors,
+      formConfig: [
+        {
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "New Floor Name",
+          required: false,
+        },
+      ],
+      defaultValues: { name: "" },
+      itemLabelCallBack: (item: IItem): string => {
+        return item.name;
+      },
+      onFormChange: (form: NgForm, valueChanged: string): void => {},
+    },
+    {
+      signlerName: "Expense",
+      id: "expenses",
+      name: "Expenses",
+      items: this.expenses,
+      formConfig: [
+        {
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "New Expense Name",
+          required: false,
+        },
+        {
+          type: "dropdown",
+          id: "floor",
+          name: "floor",
+          defaultValue: "",
+          dataProvider: () => this.floors,
+          label: "Inventory Item Related Floor",
+          required: true,
+        },
+        {
+          type: "dropdown",
+          id: "inventoryItem",
+          name: "inventoryItem",
+          defaultValue: "",
+          dataProvider: (form: NgForm) => {
+            return this.inventory.filter((i) => i.floor == form.value.floor);
+          },
+          label: "Inventory Item",
+          required: true,
+        },
+        {
+          type: "number",
+          id: "amount",
+          name: "amount",
+          defaultValue: null,
+          dataProvider: () => [],
+          label: "Amount in ₹",
+          required: true,
+        },
+      ],
+      defaultValues: { name: "" },
       itemLabelCallBack: (item: IItem): string => {
         return item.name;
       },
       onFormChange: (form: NgForm, valueChanged: string): void => {
-        if(valueChanged === 'floor'){
-          form.controls.inventoryItem.setValue('');
-        }        
-      }
+        if (valueChanged === "floor") {
+          form.controls.inventoryItem.setValue("");
+        }
+      },
     },
     {
-      signlerName: "Income", id: "income", name: "Income", items: this.incomes,
+      signlerName: "Income",
+      id: "income",
+      name: "Income",
+      items: this.incomes,
       formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "Income Name", required: true },
-        { type: "month", id: "month", name: "month", defaultValue: "", dataProvider: () => [], label: "Income Month", required: true },
-        { type: "multi-select", id: "flats", name: "flats", defaultValue: "", dataProvider: () => this.flats, label: "Flats to Pay", required: true },
-        { type: "number", id: "amount", name: "amount", defaultValue: null, dataProvider: () => [], label: "Amount in ₹", required: true },
+        {
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Income Name",
+          required: true,
+        },
+        {
+          type: "month",
+          id: "month",
+          name: "month",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Income Month",
+          required: true,
+        },
+        {
+          type: "multi-select",
+          id: "flats",
+          name: "flats",
+          defaultValue: "",
+          dataProvider: () => this.flats,
+          label: "Flats to Pay",
+          required: true,
+        },
+        {
+          type: "number",
+          id: "amount",
+          name: "amount",
+          defaultValue: null,
+          dataProvider: () => [],
+          label: "Amount in ₹",
+          required: true,
+        },
       ],
       defaultValues: {
-        name: '',
-        id: '',
-        month: '',
+        name: "",
+        id: "",
+        month: "",
         flats: [],
         amount: 0,
       },
       itemLabelCallBack: (item: IIncome): string => {
-        return `<h6 class="mb-2 pb-3 border-bottom">${(item).name}</h6>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Amount for Flat : ${new Intl.NumberFormat('en-IN').format(item.amount || 0)} ₹</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Amount for ${item.flats.length} Flats : ${new Intl.NumberFormat('en-IN').format(item.amount * item.flats.length || 0)} ₹</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Month : ${item.month}</div>
+        return `<h6 class="mb-2 pb-3 border-bottom">${item.name}</h6>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Amount for Flat : ${new Intl.NumberFormat(
+          "en-IN"
+        ).format(item.amount || 0)} ₹</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Amount for ${
+          item.flats.length
+        } Flats : ${new Intl.NumberFormat("en-IN").format(
+          item.amount * item.flats.length || 0
+        )} ₹</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Month : ${
+          item.month
+        }</div>
         <div class="p-2 m-2 border shadow-sm">
           <p class="m-0 p-0 border-bottom pb-1 mb-1">Flats Needs to Pay : </p>
-          ${item.flats.map(f => {
-          return `<div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.getItemNameById(this.flats, f)}</div>`;
-        }).join('')}
+          ${item.flats
+            .map((f) => {
+              return `<div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.getItemNameById(
+                this.flats,
+                f
+              )}</div>`;
+            })
+            .join("")}
         </div>
         `;
       },
-      onFormChange: (form: NgForm, valueChanged: string): void => {}
+      onFormChange: (form: NgForm, valueChanged: string): void => {},
     },
     {
       signlerName: "Inventory Item",
@@ -282,49 +380,138 @@ export class AppComponent {
       name: "Inventory",
       items: this.inventory,
       formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "Inventory Item Name", required: false },
-        { type: "dropdown", id: "floor", name: "floor", defaultValue: "", dataProvider: () => this.floors, label: "Item Related Floor", required: true },
-        { type: "dropdown", id: "status", name: "status", defaultValue: "", dataProvider: () => this.inventoryItemStatus, label: "Item Status", required: true },
-        { type: "number", id: "cost", name: "cost", defaultValue: null, dataProvider: () => [], label: "Item Cost Rupees", required: true },
+        {
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Inventory Item Name",
+          required: false,
+        },
+        {
+          type: "dropdown",
+          id: "floor",
+          name: "floor",
+          defaultValue: "",
+          dataProvider: () => this.floors,
+          label: "Item Related Floor",
+          required: true,
+        },
+        {
+          type: "dropdown",
+          id: "status",
+          name: "status",
+          defaultValue: "",
+          dataProvider: () => this.inventoryItemStatus,
+          label: "Item Status",
+          required: true,
+        },
+        {
+          type: "number",
+          id: "cost",
+          name: "cost",
+          defaultValue: null,
+          dataProvider: () => [],
+          label: "Item Cost Rupees",
+          required: true,
+        },
       ],
       defaultValues: {
-        name: '',
-        floor: '',
-        status: '',
+        name: "",
+        floor: "",
+        status: "",
         cost: null,
       },
       itemLabelCallBack: (item: IInventoryItem): string => {
-        return `<h6 class="mb-2 pb-2 border-bottom">${(item).name}</h6>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.floors.find(f => f.id == item.floor)?.name}</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.inventoryItemStatus.find(f => f.id == item.status)?.name}</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${new Intl.NumberFormat('en-IN').format(item.cost || 0)} ₹</div>`;
+        return `<h6 class="mb-2 pb-2 border-bottom">${item.name}</h6>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          this.floors.find((f) => f.id == item.floor)?.name
+        }</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          this.inventoryItemStatus.find((f) => f.id == item.status)?.name
+        }</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${new Intl.NumberFormat(
+          "en-IN"
+        ).format(item.cost || 0)} ₹</div>`;
       },
-      onFormChange: (form: NgForm, valueChanged: string): void => {}
+      onFormChange: (form: NgForm, valueChanged: string): void => {},
     },
     {
-      signlerName: "Vehicle", id: "Vehicles", name: "Vehicles", items: this.vehicles,
+      signlerName: "Vehicle",
+      id: "Vehicles",
+      name: "Vehicles",
+      items: this.vehicles,
       formConfig: [
-        { type: "text", id: "name", name: "name", defaultValue: "", dataProvider: () => [], label: "Vehicle Number", required: true },
-        { type: "text", id: "color", name: "color", defaultValue: "", dataProvider: () => [], label: "Vehicle Color", required: true },
-        { type: "text", id: "make", name: "make", defaultValue: "", dataProvider: () => [], label: "Vehicle Make", required: true },
-        { type: "dropdown", id: "flat", name: "flat", defaultValue: "", dataProvider: () => this.flats, label: "Vehicle Related Flat", required: true },
-        { type: "dropdown", id: "type", name: "type", defaultValue: "", dataProvider: () => this.vehicleTypes, label: "Vehicle Type", required: true },
+        {
+          type: "text",
+          id: "name",
+          name: "name",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Vehicle Number",
+          required: true,
+        },
+        {
+          type: "text",
+          id: "color",
+          name: "color",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Vehicle Color",
+          required: true,
+        },
+        {
+          type: "text",
+          id: "make",
+          name: "make",
+          defaultValue: "",
+          dataProvider: () => [],
+          label: "Vehicle Make",
+          required: true,
+        },
+        {
+          type: "dropdown",
+          id: "flat",
+          name: "flat",
+          defaultValue: "",
+          dataProvider: () => this.flats,
+          label: "Vehicle Related Flat",
+          required: true,
+        },
+        {
+          type: "dropdown",
+          id: "type",
+          name: "type",
+          defaultValue: "",
+          dataProvider: () => this.vehicleTypes,
+          label: "Vehicle Type",
+          required: true,
+        },
       ],
       defaultValues: {
-        "name": "",
-        "color": "",
-        "make": "",
-        "flat": "",
-        "type": "",
+        name: "",
+        color: "",
+        make: "",
+        flat: "",
+        type: "",
       },
       itemLabelCallBack: (item: IVehicle): string => {
-        return `<h6 class="mb-2 pb-2 border-bottom">${(item).name}</h6>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${item.color} color</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${item.make}</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.flats.find(f => f.id == item.flat)?.name}</div>
-        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${this.vehicleTypes.find(v => v.id == item.type)?.name}</div>`;
+        return `<h6 class="mb-2 pb-2 border-bottom">${item.name}</h6>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          item.color
+        } color</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          item.make
+        }</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          this.flats.find((f) => f.id == item.flat)?.name
+        }</div>
+        <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${
+          this.vehicleTypes.find((v) => v.id == item.type)?.name
+        }</div>`;
       },
-      onFormChange: (form: NgForm, valueChanged: string): void => {}
+      onFormChange: (form: NgForm, valueChanged: string): void => {},
     },
   ];
   activePage: IPage = this.pages[2];
