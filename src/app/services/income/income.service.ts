8f433f6784126fa82f3384a1d6a8d894ade00f83
vamseeuxd <vamsee.uxd@gmail.com>
2024-08-02 10:48:47 +0530

    done with service refacor

--- src/app/services/income/income.service.ts
+++ src/app/services/income/income.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
+import {
+  Firestore,
+  CollectionReference,
+  collection,
+  orderBy,
+  collectionData,
+  DocumentData,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  getDoc,
+  query,
+  Query,
+} from "@angular/fire/firestore";
 import { NgForm } from "@angular/forms";
 import { Observable, of } from "rxjs";
 import { getItemNameById, getPage, IIncome, IItem } from "src/app/interfaces";
@@ -7,7 +22,33 @@ import { getItemNameById, getPage, IIncome, IItem } from "src/app/interfaces";
   providedIn: "root",
 })
 export class IncomeService {
-  incomes$: Observable<IIncome[]> = of([
+  firestore: Firestore = inject(Firestore);
+  collection: CollectionReference = collection(this.firestore, "income");
+  queryRef = query(this.collection, orderBy("name"));
+  // prettier-ignore
+  incomes$: Observable<IIncome[]> = collectionData<IIncome>( this.queryRef as Query<IIncome, DocumentData>, { idField: "id" } );
+
+  async add(value: IIncome) {
+    delete value.id;
+    await addDoc(this.collection, value);
+  }
+
+  async update(value: IIncome, id: string) {
+    delete value.id;
+    const docRef = doc(this.firestore, `${this.collection.path}/${id}`);
+    await updateDoc(docRef, { ...value });
+  }
+
+  async remove(id: string) {
+    const docRef = doc(this.firestore, `${this.collection.path}/${id}`);
+    await deleteDoc(docRef);
+  }
+
+  async get(id: string) {
+    const docRef = doc(this.firestore, `${this.collection.path}/${id}`);
+    await getDoc(docRef);
+  }
+  oldincomes$: Observable<IIncome[]> = of([
     {
       id: "December 2024 Montly Maintainence",
       name: "December 2024 Montly Maintainence",
@@ -34,7 +75,7 @@ export class IncomeService {
     },
   ]);
   constructor() {}
-  getPage(flats: IItem[],incomes:IIncome[]) {
+  getPage(flats: IItem[], incomes: IIncome[]) {
     return getPage(
       "Income",
       "income",
@@ -93,7 +134,13 @@ export class IncomeService {
         <div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">Month : ${ item.month }</div>
         <div class="p-2 m-2 border shadow-sm"> <p class="m-0 p-0 border-bottom pb-1 mb-1">Flats Needs to Pay : </p> ${item.flats .map((f) => { return `<div class="border fs-7 d-inline-block rounded-pill px-2 me-1 mb-1">${getItemNameById( flats, f )}</div>`; }) .join("")} </div> `;
       },
-      (form: NgForm, valueChanged: string): void => {}
+      (form: NgForm, valueChanged: string): void => {},
+      {
+        add: this.add.bind(this),
+        update: this.update.bind(this),
+        remove: this.remove.bind(this),
+        get: this.get.bind(this),
+      }
     );
   }
 }
